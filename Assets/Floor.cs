d34ae7f09c095355b0ed8d75c0a3f89b0992ef25
GuilhermeCoutinho <guilhermejcoutinho@gmail.com>
2017-12-03 20:24:48 -0200

    Added floors

---
+++ Assets/Floor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+	public Sprite[] floorSprites;
+	void OnEnable () {
+		GetComponentInChildren<SpriteRenderer>().sprite = 
+		floorSprites[Random.Range(0,floorSprites.Length)];
+	}
+}
