619f99d5801e08500d08db17d18bd9610b7f2409
GuilhermeCoutinho <guilhermejcoutinho@gmail.com>
2017-12-03 16:41:58 -0200

    Added wallSpritebinding initial setup

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wall : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Initialize () {
 	}
 }
---
+++ Assets/WallSprites.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// up ; right; down;left
+public class WallSprites : SingletonMonoBehaviour<WallSprites> {
+
+	public bool resetThis ;
+	public WallSpriteBinding[] wallSpriteBindings;
+
+/* 
+	void Awake () {
+		if (resetThis){
+			for (int j=0;j<16;j++) {
+				int i = j;
+				int x, y ,z ,w;
+				x = i / 8;
+				i -= x * 8;
+				y = i/4;
+				i -= y*4;
+				z = i/2;
+				i -= z *2;
+				w = i;
+                Vector4 pos = new Vector4(x,y,z,w);
+				wallSpriteBindings[j].position = pos;
+			}
+		}
+	}
+	*/
+
+	[System.Serializable]
+	public class WallSpriteBinding{
+		public Vector4 position;
+		public Sprite sprite;
+	}
+}
