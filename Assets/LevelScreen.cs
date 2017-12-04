b76a32e91b712dab40c219ad462e34d8958f0a29
GuilhermeCoutinho <guilhermejcoutinho@gmail.com>
2017-12-04 16:32:31 -0200

    Added dynamic life counter

--- Assets/LevelScreen.cs
+++ Assets/LevelScreen.cs
@@ -11,30 +11,7 @@ public class LevelScreen : SingletonMonoBehaviour<LevelScreen> {
 		levelname =  levelScreen.GetComponentInChildren<UnityEngine.UI.Text>();
 		levelname.text = "LEVEL "+(GameManager.Instance.currentLevel+1).ToString();
 		levelScreen.SetActive(true);
-		updateLives();
 		yield return new WaitForSeconds(tempo); //black magic
 		levelScreen.SetActive(false);
-	}
-
-	public void updateLives(){
-		switch(GameManager.Instance.playerLifes){
-			case 1:
-				levelScreen.transform.GetChild(0).gameObject.SetActive(true);
-				levelScreen.transform.GetChild(1).gameObject.SetActive(false);
-				levelScreen.transform.GetChild(2).gameObject.SetActive(false);
-			break;
-			case 2:
-				levelScreen.transform.GetChild(0).gameObject.SetActive(true);
-				levelScreen.transform.GetChild(1).gameObject.SetActive(true);
-				levelScreen.transform.GetChild(2).gameObject.SetActive(false);
-			break;
-			case 3:
-				levelScreen.transform.GetChild(0).gameObject.SetActive(true);
-				levelScreen.transform.GetChild(1).gameObject.SetActive(true);
-				levelScreen.transform.GetChild(2).gameObject.SetActive(true);
-			break;
-			default:
-			break;
-		}
 	}
 }
