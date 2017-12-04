6cf13e46f4edea4096ce48c8dfd17db2f5696d1e
Enio Moura <mandosaldmer@gmail.com>
2017-12-03 22:16:44 -0200

    Added level intro screen between screens

---
+++ Assets/LevelScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScreen : SingletonMonoBehaviour<LevelScreen> {
+	public GameObject levelScreen;
+	public float tempo;
+
+	public IEnumerator BlinkScreen(){
+		UnityEngine.UI.Text levelname;
+		levelname =  levelScreen.GetComponentInChildren<UnityEngine.UI.Text>();
+		levelname.text = "LEVEL "+(GameManager.Instance.currentLevel+1).ToString();
+		levelScreen.SetActive(true);
+		updateLives();
+		yield return new WaitForSeconds(tempo); //black magic
+		levelScreen.SetActive(false);
+	}
+
+	public void updateLives(){
+		Debug.Log(levelScreen);
+		switch(GameManager.Instance.playerLifes){
+			case 1:
+				levelScreen.transform.GetChild(0).gameObject.SetActive(true);
+				levelScreen.transform.GetChild(1).gameObject.SetActive(false);
+				levelScreen.transform.GetChild(2).gameObject.SetActive(false);
+			break;
+			case 2:
+				levelScreen.transform.GetChild(0).gameObject.SetActive(true);
+				levelScreen.transform.GetChild(1).gameObject.SetActive(true);
+				levelScreen.transform.GetChild(2).gameObject.SetActive(false);
+			break;
+			case 3:
+				levelScreen.transform.GetChild(0).gameObject.SetActive(true);
+				levelScreen.transform.GetChild(1).gameObject.SetActive(true);
+				levelScreen.transform.GetChild(2).gameObject.SetActive(true);
+			break;
+			default:
+			break;
+		}
+	}
+}
